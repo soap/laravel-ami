0edef85e29b131da28d77b2ef3cc2c9a6923f665
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2021-11-13 23:55:53 +0700

    Update package again

--- src/Commands/AmiCommand.php
+++ src/Commands/AmiCommand.php
@@ -3,6 +3,7 @@
 namespace Soap\Ami\Commands;
 
 use Illuminate\Console\Command;
+use React\EventLoop\Loop;
 
 class AmiCommand extends Command
 {
@@ -12,10 +13,49 @@ class AmiCommand extends Command
 
     public function handle(): int
     {
-        $url = config('connections.ami.uri');
-        $this->info($url);
+        $this->startProcess();
         $this->comment('All done');
 
         return self::SUCCESS;
+    }
+
+    protected function startProcess($uri)
+    {
+        $factory = new Clue\React\Ami\Factory();
+
+        $factory->createClient($this->argurments('uri'))->then(function (Clue\React\Ami\Client $client) {
+            echo 'Client connected. Use STDIN to send CLI commands via asterisk AMI.' . PHP_EOL;
+            $sender = new Clue\React\Ami\ActionSender($client);
+
+            $sender->events(false);
+
+            $sender->listCommands()->then(function (Clue\React\Ami\Protocol\Response $response) {
+                echo 'Commands: ' . implode(', ', array_keys($response->getFields())) . PHP_EOL;
+            });
+
+            $client->on('close', function () {
+                echo 'Closed' . PHP_EOL;
+
+                Loop::removeReadStream(STDIN);
+            });
+
+            Loop::addReadStream(STDIN, function () use ($sender) {
+
+                $line = trim(fread(STDIN, 4096));
+
+                echo '<' . $line . PHP_EOL;
+
+                $sender->command($line)->then(
+                    function (Clue\React\Ami\Protocol\Response $response) {
+                        echo $response->getCommandOutput() . PHP_EOL;
+                    },
+                    function (Exception $error) use ($line) {
+                        echo 'Error executing "' . $line . '": ' . $error->getMessage() . PHP_EOL;
+                    }
+                );
+            });
+        }, function (Exception $error) {
+            echo 'Connection error: ' . $error;
+        });
     }
 }
