3820754f0323e34fc2732b713eb1af0ad6993fa5
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2021-11-13 23:28:32 +0700

    Update ami command

--- config/ami.php
+++ config/ami.php
@@ -1,5 +1,13 @@
 <?php
-// config for Soap/Ami
-return [
 
+return [
+    'connections' => [
+
+        "ami" => [
+            "host" => "192.168.1.58",
+            "port" => 5038,
+            "user" => "soap",
+            "secret" => "scorer1234"
+        ]
+    ]
 ];
--- src/Commands/AmiCommand.php
+++ src/Commands/AmiCommand.php
@@ -6,12 +6,14 @@ use Illuminate\Console\Command;
 
 class AmiCommand extends Command
 {
-    public $signature = 'ami:cli';
+    public $signature = 'ami:cli {uri} {command}';
 
     public $description = 'Send one command to Asterisk and exit';
 
     public function handle(): int
     {
+        $url = config('connections.ami.uri');
+        $this->info($url);
         $this->comment('All done');
 
         return self::SUCCESS;
